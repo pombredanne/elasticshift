e941cbbb761eddc8d943099ec15ecf1490f28332
Ghazni Nattarshah <ghazni.nattarshah@elasticshift.com>
2016-09-17 00:10:31 -0400

    Removed commented code.

--- core/util/uuid.go
+++ core/util/uuid.go
@@ -15,6 +15,5 @@ func NewUUID() (string, error) {
 	}
 
 	// trim the uuid without hyphen
-	//return fmt.Sprintf("%x%x%x%x%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:]), err
 	return fmt.Sprintf("%x", u[0:]), err
 }
