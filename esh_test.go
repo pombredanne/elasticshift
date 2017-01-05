c8643172542c70ddb2cf68250815134100fa3607
Ghazni Nattarshah <ghazni.nattarshah@elasticshift.com>
2017-01-04 23:01:32 -0500

    encoder decoder test case added.

---
+++ esh_test.go
@@ -0,0 +1,10 @@
+// Package esh
+// Author Ghazni Nattarshah
+// Date: Jan 4, 2017
+package esh
+
+type Testcase struct  {
+	In interface{}
+	Out interface{}
+	Err error
+}
---
+++ user_decoder_test.go
@@ -0,0 +1,54 @@
+// Package esh
+// Author Ghazni Nattarshah
+// Date: Jan 4, 2017
+package esh
+
+import (
+	"testing"
+	"github.com/stretchr/testify/suite"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"bytes"
+	"context"
+)
+
+func TestUserDecoder(t *testing.T) {
+	suite.Run(t, new(UserDecoderTestSuite))
+}
+
+type UserDecoderTestSuite struct {
+	suite.Suite
+}
+
+func (suite *UserDecoderTestSuite) TestSignup() {
+
+	sr  := signupRequest{Fullname:"GN", Email:"g.n@gmai.com", Password:"Pwd", Team:"testteam"}
+	sr2 := signupRequest{Fullname:"GN", Email:"g.n@gmai.com", Password:"Pwd"}
+	sr3 := signupRequest{Fullname:"GN", Email:"g.n@gmai.com", Password:"Pwd", Team:"elasticshift"}
+	testcases := []Testcase{
+		{sr, sr, nil},
+		{sr2, sr3, nil},
+		{sr3, sr3, nil},
+	}
+
+	ctx := context.TODO()
+	ctx = context.WithValue(ctx, "subdomain", "elasticshift")
+
+	for _, testcase := range testcases {
+
+		data, err := json.Marshal(testcase.In)
+		assert.Nil(suite.T(), err)
+
+		req := httptest.NewRequest("POST", "http://example.com/foo", bytes.NewBuffer(data))
+		res, err := decodeSignUpRequest(ctx, req)
+
+		assert.ObjectsAreEqual(testcase.Err, err)
+		assert.Equal(suite.T(), testcase.Out, res)
+	}
+
+	req := httptest.NewRequest("POST", "http://example.com/foo", bytes.NewBuffer([]byte{0}))
+	res, err := decodeSignUpRequest(ctx, req)
+	assert.NotNil(suite.T(), err)
+	assert.ObjectsAreEqual(false, res)
+}
