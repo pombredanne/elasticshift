226ef1a8e980e155b94d80c961cc2b6fbfad5ee4
Ghazni Nattarshah <ghazni.nattarshah@elasticshift.com>
2018-06-05 07:03:52 +0530

    script updates.

--- scripts/worker/startup.sh
+++ scripts/worker/startup.sh
@@ -2,8 +2,8 @@
 ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime
 
 # create elasticshift user/group
-groupadd --gid 1002 elasticshift \
-	&& useradd --uid 1002 --gid elasticshift --shell /bin/bash --create-home elasticshift \
+groupadd --gid 1005 elasticshift \
+	&& useradd --uid 1005 --gid elasticshift --shell /bin/bash --create-home elasticshift \
 	&& echo 'elasticshift ALL=NOPASSWD: ALL' >> /etc/sudoers.d/elasticshift
 
 su - elasticshift
