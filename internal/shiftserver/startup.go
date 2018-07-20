f68567f57f7a98e2b8bea00ee9798d089c679349
Ghazni Nattarshah <ghazni.nattarshah@elasticshift.com>
2018-07-20 21:26:21 +0530

    Used logger from internal package.

--- internal/shiftserver/startup.go
+++ internal/shiftserver/startup.go
@@ -10,15 +10,14 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"time"
 
 	"log"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/justinas/alice"
+	"gitlab.com/conspico/elasticshift/internal/pkg/logger"
 	"gitlab.com/conspico/elasticshift/internal/shiftserver/store"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
@@ -148,7 +147,7 @@ func Run() error {
 
 	sc := ServerConfig{}
 	// logger
-	logger, err := newLogger(c.Logger.Level, c.Logger.Format)
+	logger, err := logger.New(c.Logger.Level, c.Logger.Format)
 	if err != nil {
 		return fmt.Errorf("invalid config: %v", err)
 	}
@@ -277,48 +276,4 @@ func Run() error {
 	}()
 
 	return <-errch
-}
-
-var (
-	logLevels  = []string{"debug", "info", "error"}
-	logFormats = []string{"json", "text"}
-)
-
-type utcFormatter struct {
-	f logrus.Formatter
-}
-
-func (f *utcFormatter) Format(e *logrus.Entry) ([]byte, error) {
-	e.Time = e.Time.UTC()
-	return f.f.Format(e)
-}
-
-func newLogger(level string, format string) (logrus.Logger, error) {
-	var logLevel logrus.Level
-	switch strings.ToLower(level) {
-	case "debug":
-		logLevel = logrus.DebugLevel
-	case "", "info":
-		logLevel = logrus.InfoLevel
-	case "error":
-		logLevel = logrus.ErrorLevel
-	default:
-		return logrus.Logger{}, fmt.Errorf("log level is not one of the supported values (%s): %s", strings.Join(logLevels, ", "), level)
-	}
-
-	var formatter utcFormatter
-	switch strings.ToLower(format) {
-	case "", "text":
-		formatter.f = &logrus.TextFormatter{DisableColors: true}
-	case "json":
-		formatter.f = &logrus.JSONFormatter{}
-	default:
-		return logrus.Logger{}, fmt.Errorf("log format is not one of the supported values (%s): %s", strings.Join(logFormats, ", "), format)
-	}
-
-	return logrus.Logger{
-		Out:       os.Stderr,
-		Formatter: &formatter,
-		Level:     logLevel,
-	}, nil
 }
