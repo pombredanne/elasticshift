cf57a9d4359562079a99cf092730c7ee6de5bc07
Ghazni Nattarshah <ghazni.nattarshah@elasticshift.com>
2018-12-22 20:30:47 +0530

    Removed unnecessary files.

--- wl.sh
+++
@@ -1,5 +0,0 @@
-echo "Building worker.."
-CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o ./bin/worker/worker -a -tags netgo -ldflags '-s -w' ./cmd/worker/worker.go
-
-echo "copying to storage"
-sudo cp ./bin/worker/worker /lab/elasticshift/sys
